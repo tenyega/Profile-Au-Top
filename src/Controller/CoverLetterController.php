154c9b237d63017004a647500c9ac76a8fa3aa33
tenyega <tenyega23@gmail.com>
2024-10-02 15:35:49 +0200

    coverLetterController done

--- src/Controller/CoverLetterController.php
+++ src/Controller/CoverLetterController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\CoverLetterRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 class CoverLetterController extends AbstractController
 {
-    #[Route('/cover/letter', name: 'app_cover_letter')]
-    public function index(): Response
+    #[Route('/cover-letter/g/generate', name: 'app_cover_letter', methods: ['POST'])]
+    public function generate(): Response
     {
-        return $this->render('cover_letter/index.html.twig', [
-            'controller_name' => 'CoverLetterController',
+        return $this->render('cover_letter/generate.html.twig');
+    }
+
+    #[Route('/cover-letter/{id}', name: 'app_cover_letter_show', methods: ['GET'])]
+    public function show(CoverLetterRepository $clr, string $id): Response
+    {
+        $coverLetter = $clr->findOneBy(['id' => $id]);
+        return $this->render('cover_letter/show.html.twig', [
+            'coverLetter' => $coverLetter
         ]);
     }
 }
